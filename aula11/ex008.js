1e867efa122d97085ee2924b25486ed8794002ea
Leonardo Caridade Lindo <100426910+leocaridade@users.noreply.github.com>
2022-05-17 16:51:15 -0400

    js update

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60) { //Condicao simples
+    console.log(`Voce ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de seguranca`)
